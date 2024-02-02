ad9da53f0d1508e6f6107ffc03d2a47bc340b478
sarrrovv <ahmetkoca2395@gmail.com>
2024-02-02 21:06:23 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,11 @@
 package org.example;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     static Scanner scanner = new Scanner(System.in);
+    static Random random = new Random();
     static int[][][][] table = {
             {{{0,0,0},{0,0,0},{0,0,0}},{{0,0,0},{0,0,0},{0,0,0}},{{0,0,0},{0,0,0},{0,0,0}}},
             {{{0,0,0},{0,0,0},{0,0,0}},{{0,0,0},{0,0,0},{0,0,0}},{{0,0,0},{0,0,0},{0,0,0}}},
@@ -11,6 +13,7 @@ public class Main {
     };
 
     public static void main(String[] args) {
+        generateSudoku();
         play();
     }
     public static void play(){
@@ -22,6 +25,7 @@ public class Main {
         int i =0;
         int k = 0;
         int y = 1;
+        int x = 1;
 
         System.out.println("   a b c d e f g h i");
 
@@ -30,10 +34,10 @@ public class Main {
                 System.out.print(y+ "  ");
                 y++;
                 for (int j = 0; j < table[i].length; j++) {
-
                     for (int l = 0; l < table[i][j][k].length; l++) {
-                        System.out.print(table[i][j][k][l]+" ");
+                        System.out.print(table[i][j][k][l] + " ");
                     }
+
                 }
                 System.out.println();
                 k++;
@@ -42,6 +46,8 @@ public class Main {
             i++;
         }while (i < table.length);
     }
+
+
     public static void changeNumber(){
         try {
             System.out.println("Sayıyı değiştirmek için önce sütun sonra satır değerini giriniz : ");
@@ -52,7 +58,8 @@ public class Main {
             int col = script.toLowerCase().indexOf(column.toLowerCase());
             System.out.println("Sayı'nın kaç olmasını istiyorsunuz");
             int number = scanner.nextInt();
-            if(isSuitable(number,col,row)) table[row<3 ? 0: row<6 ? 1:2][col<3 ? 0: col<6 ? 1:2][row%3][col%3] = number;
+            if(isSuitable(number,(row < 3 ? 0 : row < 6 ? 1 : 2),(col < 3 ? 0 : col < 6 ? 1 : 2),(col%3),(row % 3)))
+                table[row<3 ? 0: row<6 ? 1:2][col<3 ? 0: col<6 ? 1:2][row%3][col%3] = number;
             else System.out.println("Hatalı giriş lütfen tekrar deneyiniz.");
             scanner.nextLine();
             play();
@@ -61,29 +68,64 @@ public class Main {
             changeNumber();
         }
     }
-    static boolean isSuitable(int number, int col, int row){
+
+    public static void generateSudoku(){
+        int difficulty = 39;
+        int maxValue = 81;
+
+        int i =0;
+        int k = 0;
+        do{
+            for (int m = 0; m < table.length; m++) {
+                for (int j = 0; j < table[i].length; j++) {
+                    for (int l = 0; l < table[i][j][k].length; l++) {
+                        int fill = random.nextInt(maxValue)+1;
+                        if(fill > difficulty && fill <= maxValue){
+                            maxValue--;
+                        }
+                        else if(fill <= difficulty){
+                            int number;
+                            do {
+                                number = random.nextInt(9)+1;
+                            } while (!isSuitable(number,i,j, k, l));
+                            table[i][j][l][k] = number;
+                            maxValue--;
+                            difficulty--;
+                        }
+                    }
+
+                }
+                k++;
+            }
+            k = 0;
+            i++;
+        }while (i < table.length);
+
+    }
+    static boolean isSuitable(int number,int blockRow,int block, int col, int row){
         if(number<10 && number>0){
         for (int i = 0; i < 3; i++) {
+            String s = blockRow + "-" + block + "-" + row + "-" + col;
+
             for (int j = 0; j < 3; j++) {
-                if(!(((row < 3 ? 0 : row < 6 ? 1 : 2) + "-" + (col < 3 ? 0 : col < 6 ? 1 : 2) + "-" + (row % 3) + "-" + (col % 3))
-                        .equals(i + "-" + (col < 3 ? 0 : col < 6 ? 1 : 2) + "-" + j + "-" + (col%3)))){
-                    if(table[i][col<3 ? 0: col<6 ? 1:2][j][col%3]==number) return false;
+
+                if(!(s.equals(i + "-" + block + "-" + j + "-" + col))){
+                    if(table[i][block][j][col]==number) return false;
                 }
             }
             for (int j = 0; j < 3; j++) {
-                if(!(((row < 3 ? 0 : row < 6 ? 1 : 2) + "-" + (col < 3 ? 0 : col < 6 ? 1 : 2) + "-" + (row % 3) + "-" + (col % 3))
-                        .equals((row<3 ? 0: row<6 ? 1:2)+"-"+i+"-"+(row%3)+"-"+j))){
-                    if(table[row<3 ? 0: row<6 ? 1:2][i][row%3][j]==number) return false;
+                if(!(s.equals((blockRow)+"-"+i+"-"+row+"-"+j))){
+                    if(table[blockRow][i][row][j]==number) return false;
                 }
             }
             for (int j = 0; j < 3; j++) {
-                if(!(((row < 3 ? 0 : row < 6 ? 1 : 2) + "-" + (col < 3 ? 0 : col < 6 ? 1 : 2) + "-" + (row % 3) + "-" + (col % 3))
-                        .equals((row<3 ? 0: row<6 ? 1:2)+"-"+(col < 3 ? 0 : col < 6 ? 1 : 2)+"-"+i+"-"+j))){
-                    if(table[row<3 ? 0: row<6 ? 1:2][(col < 3 ? 0 : col < 6 ? 1 : 2)][i][j]==number) return false;
+                if(!(s.equals((blockRow)+"-"+block+"-"+i+"-"+j))){
+                    if(table[blockRow][block][i][j]==number) return false;
                 }
             }
         }
         }else return false;
+
         return true;
     }
 }
